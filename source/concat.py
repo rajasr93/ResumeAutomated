3785c5a848900dc2041940f60170e081b6e19ed6
Rishabh Sharma <59526954+ris3abh@users.noreply.github.com>
2024-03-27 16:42:39 -0400

    structuring the project

--- source/concat.py
+++ source/concat.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import os
 
 # from the data folder, reading all the files starting with jobs:
-files = [f for f in os.listdir('data') if f.startswith('jobs')]
+files = [f for f in os.listdir('../data') if f.startswith('jobs')]
 # reading all the files into a list of dataframes
 dfs = [pd.read_csv(os.path.join('data', f)) for f in files]
 # concatenating all the dataframes into one
@@ -14,4 +14,4 @@ print(df.shape)
 # only need to keep title, company, location and description columns
 df = df[['title', 'company', 'location', 'description']]
 
-df.to_csv('finalized_data/data_security_jobs.csv', index=False)
+df.to_csv('../finalized_data/data_security_jobs.csv', index=False)
