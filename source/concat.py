139c84ad6046fcf0dde375604207623872f58cc9
rishikax <lallrishika@gmail.com>
2024-03-27 22:04:23 -0400

    Added Business anaylst and financial analyst jobs

--- source/concat.py
+++ source/concat.py
@@ -4,12 +4,12 @@ import os
 # from the data folder, reading all the files starting with jobs:
 files = [f for f in os.listdir('../data') if f.startswith('jobs')]
 # reading all the files into a list of dataframes
-dfs = [pd.read_csv(os.path.join('data', f)) for f in files]
-# concatenating all the dataframes into one
+dfs = [pd.read_csv(os.path.join('../data', f)) for f in files]
+# # concatenating all the dataframes into one
 df = pd.concat(dfs)
 print(df.shape)
 
 # only need to keep title, company, location and description columns
 df = df[['title', 'company', 'location', 'description']]
 
-df.to_csv('../finalized_data/data_security_jobs.csv', index=False)
+df.to_csv('../finalized_data/financial_analyst_job.csv', index=False)
